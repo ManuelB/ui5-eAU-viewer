5ea8470ba53e58fa4371020cea53515896ad0b01
Pablo Escaida <1pablo.escaida1@gmail.com>
2022-12-20 20:13:17 +0100

    It is searching / filtering but I dont know what

--- webapp/controller/Patients.controller.js
+++ webapp/controller/Patients.controller.js
@@ -1,12 +1,29 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function(Controller) {
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/model/Filter",
+	"sap/ui/model/FilterOperator"
+], function(Controller, Filter, FilterOperator) {
 	"use strict";
 
 	return Controller.extend("myhealthapp.controller.Patients", {
 
         onInit: function(){
 			this.initializeRouter();
+		},
+
+		onSearchPatients : function (oEvent) {
+
+			// build filter array
+			var aFilter = [];
+			var sQuery = oEvent.getParameter("query");
+			if (sQuery) {
+				aFilter.push(new Filter("FirstName", FilterOperator.Contains, sQuery));
+			}
+
+			// filter binding
+			var oList = this.byId("patientsList");
+			var oBinding = oList.getBinding("items");
+			oBinding.filter(aFilter);
 		},
 
 		initializeRouter: function(){
@@ -20,7 +37,7 @@ sap.ui.define([
 		onPressPatient : function(oEvent){
 			var sPatientId = this.getView().getModel().getProperty(oEvent.getParameter("listItem").getBindingContextPath()+"/id")
 			this.oRouter.navTo("patientRoute", {patientId: sPatientId});
-		}
+		},
 
 	});
 });

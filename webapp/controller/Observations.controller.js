058a7a7256a9805c916e06cfbbabd7ae87481bdf
Pablo Escaida <1pablo.escaida1@gmail.com>
2022-12-22 14:06:04 +0100

    Abgabeversion 14:00

--- webapp/controller/Observations.controller.js
+++ webapp/controller/Observations.controller.js
@@ -1,12 +1,29 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function(Controller) {
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/model/Filter",
+	"sap/ui/model/FilterOperator"
+], function(Controller, Filter, FilterOperator) {
 	"use strict";
 
 	return Controller.extend("myhealthapp.controller.Observations", {
 
         onInit: function(){
 			this.initializeRouter();
+		},
+
+		onSearchObservations : function (oEvent) {
+
+			// build filter array
+			var aFilter = [];
+			var sQuery = oEvent.getParameter("query");
+			if (sQuery) {
+				aFilter.push(new Filter("decription", FilterOperator.Contains, sQuery));
+			}
+
+			// filter binding
+			var oList = this.byId("observationsList");
+			var oBinding = oList.getBinding("items");
+			oBinding.filter(aFilter);
 		},
 
 		initializeRouter: function(){
